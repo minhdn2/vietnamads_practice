340680fb0e299af5974f3cbc5db743cfaa94fa75
Minh Duong <minhduongnhat@outlook.com>
2019-07-11 01:18:53 +0700

    update something

--- Models/Base/ConstantData.cs
+++ Models/Base/ConstantData.cs
@@ -9,11 +9,13 @@ namespace VietnamAds_Practice.Models.Base
     {
         public static List<string> Price_UnitOfMeasure;
         public static List<string> Ads_Nature;
+        public static List<string> Size_UnitOfMeasure;
 
         static ConstantData()
         {
             Price_UnitOfMeasure = new List<string> { "VND", "USD" };
             Ads_Nature = new List<string> { "Tĩnh", "Động" };
+            Size_UnitOfMeasure = new List<string> { "Milimeters", "Centimeters", "Meters" };
         }
     }
 }
